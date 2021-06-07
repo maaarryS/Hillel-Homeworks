d2a89ab7fa587c76111b70543d2184e686f962b2
Mariia <culebra404@gmail.com>
2021-06-07 23:32:50 +0300

    homework10 was modified

--- homework10/Форма логина (Dom Events)/functions.js
+++ homework10/Форма логина (Dom Events)/functions.js
@@ -5,42 +5,7 @@ const USERS = [
   { login: "user", password: "pass123" }
 ]
 
-const loginEl = document.getElementById("login"),
-      pswdEl = document.getElementById("password"),
-      mainEl = document.getElementById("main"),
-      btnEl = document.getElementById("btn"),
-      msgEls = document.getElementsByClassName("result-message");
-
 function checkCredentials(userName, password) {
   console.log(userName, password);
   return !!USERS.find(e => e.login === userName && e.password === password);
-}
-
-function checkData(userName, password) {
-    if (checkCredentials(userName, password)) {
-      let successMsg = msgEls[0];
-      successMsg.removeAttribute("hidden", "true");
-      successMsg.classList.remove("hidden");
-      successMsg.id = "success";
-      successMsg.textContent = "Logged in";
-      mainEl.innerHTML = null;
-      mainEl.append(successMsg);
-  
-      console.log("success");
-    } else {
-      pswdEl.value = "";
-      let failMsg = msgEls[0];
-      failMsg.removeAttribute("hidden", "true");
-      failMsg.classList.remove("hidden");
-      failMsg.id = "fail";
-      failMsg.textContent = "Failed to verify user. Try again";
-      mainEl.append(failMsg);
-    };
-  
-}
-
-function enableBtn(userName, password) {
-  if (userName && password) {
-    btnEl.removeAttribute("disabled", "true");
-  }
 }
--- homework10/Форма логина (Dom Events)/main.js
+++ homework10/Форма логина (Dom Events)/main.js
@@ -1,6 +1,40 @@
 (function() {
   let userName, password;
 
+  const loginEl = document.getElementById("login"),
+        pswdEl = document.getElementById("password"),
+        mainEl = document.getElementById("main"),
+        btnEl = document.getElementById("btn"),
+        msgEls = document.getElementsByClassName("result-message");
+
+  function checkData(userName, password) {
+    if (checkCredentials(userName, password)) {
+      let successMsg = msgEls[0];
+      successMsg.removeAttribute("hidden", "true");
+      successMsg.classList.remove("hidden");
+      successMsg.id = "success";
+      successMsg.textContent = "Logged in";
+      mainEl.innerHTML = null;
+      mainEl.append(successMsg);
+  
+      console.log("success");
+    } else {
+      pswdEl.value = "";
+      let failMsg = msgEls[0];
+      failMsg.removeAttribute("hidden", "true");
+      failMsg.classList.remove("hidden");
+      failMsg.id = "fail";
+      failMsg.textContent = "Failed to verify user. Try again";
+      mainEl.append(failMsg);
+    }; 
+  }
+  
+  function enableBtn(userName, password) {
+    if (userName && password) {
+      btnEl.removeAttribute("disabled", "true");
+    }
+  }
+
   loginEl.addEventListener("input", (evt) => {
   userName = evt.target.value;
   enableBtn(userName, password);
