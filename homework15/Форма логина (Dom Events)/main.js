3701d95ed9b0defa7fc92edc525d6109b60d9bbb
Mariia <culebra404@gmail.com>
2021-07-08 23:43:52 +0300

    homework 15 was added

---
+++ homework15/Форма логина (Dom Events)/main.js
@@ -0,0 +1,72 @@
+(function() {
+  let userName, password;
+
+  const loginEl = document.getElementById("login"),
+        pswdEl = document.getElementById("password"),
+        mainEl = document.getElementById("main"),
+        btnEl = document.getElementById("btn"),
+        msgEls = document.getElementsByClassName("result-message");
+
+  
+  function enableBtn(userName, password) {
+    if (userName && password) {
+      btnEl.removeAttribute("disabled", "true");
+    }
+  }
+
+  loginEl.addEventListener("input", (evt) => {
+  userName = evt.target.value;
+  enableBtn(userName, password);
+  });
+
+  pswdEl.addEventListener("input", (evt) => {
+  password = evt.target.value;
+  enableBtn(userName, password);
+  });
+
+  btnEl.addEventListener("click", () => {
+    const userName = loginEl.value;
+    const password = pswdEl.value;
+
+    checkCredentials(userName, password, 
+      (e) => {
+      let successMsg = msgEls[0];
+      successMsg.removeAttribute("hidden", "true");
+      successMsg.classList.remove("hidden");
+      successMsg.id = "success";
+      successMsg.textContent = "Logged in";
+      mainEl.innerHTML = null;
+      mainEl.append(successMsg);
+  
+      console.log("success");
+    }, 
+    (e) => {
+      pswdEl.value = "";
+      let failMsg = msgEls[0];
+      failMsg.removeAttribute("hidden", "true");
+      failMsg.classList.remove("hidden");
+      failMsg.id = "fail";
+      failMsg.textContent = "Failed to verify user. Try again";
+      mainEl.append(failMsg);
+    }
+    )
+  });
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
